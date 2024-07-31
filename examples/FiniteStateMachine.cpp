e6422258dde97c771197c269f4eca339311cfff4
Michaël <michaeltravail@hotmail.com>
2024-07-31 21:19:25 +0200

    init

---
+++ examples/FiniteStateMachine.cpp
@@ -0,0 +1,77 @@
+﻿#include "FiniteStateMachine.h"
+
+
+//States
+struct Idle {};
+struct Processing { std::string task; };
+struct Finished {};
+
+//Events
+class Start {};
+class ChangeTask {};
+class Complete {};
+class Reset {};
+
+class StateMachineImpl : public StateMachine<StateMachineImpl, Idle, Processing, Finished>
+{
+public:
+    StateMachineImpl() : StateMachine<StateMachineImpl, Idle, Processing, Finished>(Idle{}) {}
+
+    //Graph
+    void handle_event(Start const& event, Idle const&) {
+        this->transition_to(Processing{ "Task1" });
+    }
+
+    void handle_event(Complete const& event, Processing const&) {
+        this->transition_to(Finished{});
+    }
+
+    void handle_event(ChangeTask const& event, Processing& state) {
+        state.task = "Task2";
+    }
+
+    void handle_event(Reset const& event, Finished const&) {
+        this->transition_to(Idle{});
+    }
+
+    //Stopgap
+    template<typename Event, typename State>
+    void handle_event(Event const& event, State const& state) {
+        throw std::runtime_error(std::string("Unhandled event ") + typeid(event).name() + " in State " + typeid(state).name());
+    }
+};
+
+class StatePrinter
+{
+public:
+    void operator()(Idle const&) {
+        std::cout << "State: Idle" << std::endl;
+    }
+
+    void operator()(Processing const& state) {
+        std::cout << "State: Processing (" << state.task << ")" << std::endl;
+    }
+
+    void operator()(Finished const&) {
+        std::cout << "State: Finished" << std::endl;
+    }
+};
+
+int main() {
+    StateMachineImpl fsm;
+
+    StatePrinter printer;
+    fsm.visit(printer);
+    fsm.safe_handle_event(Start{});
+    fsm.visit(printer);
+    fsm.safe_handle_event(ChangeTask{});
+    fsm.visit(printer);
+    fsm.safe_handle_event(Complete{});
+    fsm.visit(printer);
+    fsm.safe_handle_event(Start{});
+    fsm.visit(printer);
+    fsm.safe_handle_event(Reset{});
+    fsm.visit(printer);
+
+    return 0;
+}
---
+++ include/FiniteStateMachine.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+#include <iostream>
+#include <variant>
+#include <string>
+
+template<typename T, typename... Ts>
+struct is_one_of;
+
+template<typename T>
+struct is_one_of<T> : std::false_type {};
+
+template<typename T, typename First, typename... Rest>
+struct is_one_of<T, First, Rest...>
+    : std::conditional_t<std::is_same_v<T, First>, std::true_type, is_one_of<T, Rest...>> {};
+
+template<typename T, typename... Ts>
+concept OneOf = is_one_of<T, Ts...>::value;
+
+template <typename Event, typename Child, typename State>
+concept HasHandleEventForState = requires(Event const& event, Child & child, State & state) {
+    { child.handle_event(event, state) } -> std::same_as<void>;
+};
+
+template <typename Event, typename Child, typename ... States>
+concept HasHandleEvent = (HasHandleEventForState<Event, Child, States> && ...);
+
+template<typename Func, typename State>
+concept ReadOnlyStateFunctionForState = requires(Func && func, State const& state) {
+    { func(state) };
+};
+
+template<typename Func, typename ... States>
+concept ReadOnlyStateFunction = (ReadOnlyStateFunctionForState<Func, States> && ...);
+
+template<typename Child, typename ... States>
+class StateMachine {
+    using State = std::variant<States...>;
+public:
+    template<OneOf<States...> State>
+    StateMachine(State&& initState) : state_(std::forward<State>(initState)) {}
+
+    template <OneOf<States...> NewState>
+    void transition_to(NewState&& new_state) {
+        state_ = std::forward<NewState>(new_state);
+    }
+
+    template<HasHandleEvent<Child, States...> Event>
+    void handle_event(Event const& event) {
+        std::visit([this, &event](auto& state) { static_cast<Child*>(this)->handle_event(event, state); }, state_);
+    }
+
+    template<ReadOnlyStateFunction<States...> Func>
+    void visit(Func&& func) {
+        std::visit(func, state_);
+    }
+
+    template<HasHandleEvent<Child, States...> Event>
+    bool safe_handle_event(Event const& event) {
+        try
+        {
+            this->handle_event(event);
+            return true;
+        }
+        catch (std::exception const& e)
+        {
+            std::cout << e.what() << std::endl;
+            return false;
+        }
+    }
+
+private:
+    State state_;
+};
+
+// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
